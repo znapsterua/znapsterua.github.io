54a59036e1ca93753b85cb7c0a31b427d8091e0b
znapsterua <zartem_ua@mail.ru>
2016-09-23 16:28:06 +0300

    test
    test

--- production/test_js/assets/js/bundle.js
+++ production/test_js/assets/js/bundle.js
@@ -44,6 +44,64 @@ Valid.prototype.ValidMail = function() {
 };
 
 Valid.prototype.ValidPass = function() {
+    var password = document.getElementById('pass').value,
+        cover = document.querySelectorAll('.pass'),
+        title = document.querySelectorAll('.pass_t'),
+        s_letters = "qwertyuiopasdfghjklzxcvbnm",
+        b_letters = "QWERTYUIOPLKJHGFDSAZXCVBNM",
+        digits = "0123456789",
+        specials = "!@#$%^&*()_-+=\|/.,:;[]{}",
+        is_s = false,
+        is_b = false,
+        is_d = false,
+        is_sp = false;
+    for (var i = 0; i < password.length; i++) {
+      if (!is_s && s_letters.indexOf(password[i]) != -1) is_s = true;
+      else if (!is_b && b_letters.indexOf(password[i]) != -1) is_b = true;
+      else if (!is_d && digits.indexOf(password[i]) != -1) is_d = true;
+      else if (!is_sp && specials.indexOf(password[i]) != -1) is_sp = true;
+    }
+    var rating = 0;
+    var text = "";
+    if (is_s) rating++;
+    if (is_b) rating++;
+    if (is_d) rating++;
+    if (is_sp) rating++;
+    if (password.length < 6 && rating < 3){
+        title[0].style.background = '#f8f8f8';
+        title[1].style.background = '#f8f8f8';
+        cover[0].style.border = 'solid 1px red';
+        cover[1].style.border = 'solid 1px red';
+        text = "Простой пароль";
+        alert(text);
+    }
+    else if (password.length < 6 && rating >= 3){
+        this.TheSame();
+    }
+    else if (password.length >= 8 && rating < 3){
+        this.TheSame();
+    }
+    else if (password.length >= 8 && rating >= 3){
+        this.TheSame();
+    }
+    else if (password.length >= 6 && rating == 1){
+        title[0].style.background = '#f8f8f8';
+        title[1].style.background = '#f8f8f8';
+        cover[0].style.border = 'solid 1px red';
+        cover[1].style.border = 'solid 1px red';
+        text = "Простой пароль";
+        alert(text);
+    }
+    else if (password.length >= 6 && rating > 1 && rating < 4){
+        this.TheSame();
+    }
+    else if (password.length >= 6 && rating == 4){
+        this.TheSame();
+    }
+    return false;
+};
+
+Valid.prototype.TheSame = function() {
     var pass = document.getElementById('pass').value,
         conf = document.getElementById('conf').value;
         cover = document.querySelectorAll('.pass'),
