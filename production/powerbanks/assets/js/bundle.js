2fe2081e881bed6d129f7cca4db44c24b9b1550d
znapsterua <zartem_ua@mail.ru>
2016-10-04 15:56:18 +0300

    test
    test

--- production/powerbanks/assets/js/bundle.js
+++ production/powerbanks/assets/js/bundle.js
@@ -180,8 +180,10 @@ Modals.prototype.show = function() {
         $('#dark').remove();
     }
     $('body').append('<div id="dark"></div>');
-    // document.getElementById("play1").setAttribute('src', this.src1);
-    // document.getElementById("play2").setAttribute('src', this.src2);
+    if(this.link == document.querySelectorAll('.modal-video-f') || this.link == document.querySelectorAll('.modal-video-s')){
+	    document.getElementById("play1").setAttribute('src', this.src1);
+	    document.getElementById("play2").setAttribute('src', this.src2);
+	}
     for (var i = 0; i <= document.querySelector('.modals').children.length-1; i++) {
         document.querySelector('.modals').children[i].style.display = 'none';
     }
@@ -193,8 +195,10 @@ Modals.prototype.show = function() {
 };
 
 Modals.prototype.closeF = function() {
-    // document.getElementById("play1").setAttribute('src', '');
-    // document.getElementById("play2").setAttribute('src', '');
+	if(this.link == document.querySelectorAll('.modal-video-f') || this.link == document.querySelectorAll('.modal-video-s')){
+	    document.getElementById("play1").setAttribute('src', '');
+    	document.getElementById("play2").setAttribute('src', '');
+	}
     $('#dark').remove();
     this.modal.style.display = 'none';
 };
