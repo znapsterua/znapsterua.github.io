dcdbd313dd357330db56f37fa4633c50d17aecc7
znapsterua <zartem_ua@mail.ru>
2016-12-02 15:11:32 +0200

    test
    test

--- production/project/assets/js/bundle.js
+++ production/project/assets/js/bundle.js
@@ -67,3 +67,69 @@ function Modal(show, hide, modal) {
         }
 
         new Modal($('#show'), $('#hide'), $('.modal'));
+
+
+
+var i = 0, 
+rotate = 0; 
+if (window.addEventListener) { 
+if ('onwheel' in document) { 
+// IE9+, FF17+, Ch31+ 
+window.addEventListener("wheel", onWheel); 
+} else if ('onmousewheel' in document) { 
+// устаревший вариант события 
+window.addEventListener("mousewheel", onWheel); 
+} else { 
+// Firefox < 17 
+window.addEventListener("MozMousePixelScroll", onWheel); 
+} 
+} else { // IE8- 
+window.attachEvent("onmousewheel", onWheel); 
+} 
+
+function onWheel(event){ 
+var e = event; 
+if(e.deltaY < 0 && i != 0){ 
+i--; 
+console.log(e.deltaY); 
+Ajax(i); 
+}else if(i != 8 && e.deltaY > 0){ 
+i++; 
+Ajax(i); 
+} 
+} 
+var rotate = parseInt(-45);
+
+function Ajax(i){ 
+var i = i; 
+console.log(i);
+if (i == 0 || i == 8) {
+   $('#display').fadeOut();
+} else{
+    $('#display').fadeIn();
+    $('#display').css('display', 'flex')
+}
+function getRandomInt(min, max) { 
+return Math.floor(Math.random() * (max - min)) + min; 
+} 
+$('#wrap').empty(); 
+$('#wrap').css('display', 'none'); // по окончанию загрузки страницы 
+// $('#example-1').click(function(){ // вешаем на клик по элементу с id = example-1 
+// $(document).ready(function(){ 
+// по окончанию загрузки страницы 
+$.ajax({ 
+url: 'page'+ i +'.html', // указываем URL и 
+cache: false, 
+beforeSend: function(){ 
+rotate += 360; 
+$('#line').css('transform', 'rotate('+ rotate +'deg)'); 
+$('#line').css('background', 'rgb('+ getRandomInt(0, 255) + ',' + getRandomInt(0, 255) + ',' + getRandomInt(0, 255) +')'); 
+
+}, 
+success: function(html) { 
+$('#wrap').html(html).fadeIn(500); 
+} 
+}); 
+// }); 
+// }); 
+};
