c2126a361afd25bf4385490939d89c736b59996d
znapsterua <zartem_ua@mail.ru>
2016-12-10 07:39:25 +0200

    test
    test

--- production/some/assets/js/bundle.js
+++ production/some/assets/js/bundle.js
@@ -32,7 +32,7 @@ function Modal(show, hide, modal) {
                 console.log(i); 
                 $('.content__img')[i].style.opacity = 1; 
                 $('.content__img')[i].style.transform = 'scale(1, 1)'; 
-                if (i != $('.content__img').length-1) { 
+                if (i != 5) { 
                     i++; 
                     that.animate(i); 
                 } else { 
@@ -54,7 +54,7 @@ function Modal(show, hide, modal) {
                 $('.show_hide').fadeIn();
                 $('.project__box').css('transform', 'scale(1, 1)');
             },500);
-            for (var i = 0; i < $('.content__img').length; i++) {
+            for (var i = 0; i < 6; i++) {
                 $('.content__img')[i].style.opacity = 0; 
                 $('.content__img')[i].style.transform = 'scale(.7, .7)'; 
             }
@@ -71,53 +71,117 @@ function Modal(show, hide, modal) {
 
 
 var i = 0,
-wheel = 0; 
-rotate = 0; 
-if (window.addEventListener) { 
-if ('onwheel' in document) { 
-// IE9+, FF17+, Ch31+ 
-window.addEventListener("wheel", onWheel); 
-} else if ('onmousewheel' in document) { 
-// устаревший вариант события 
-window.addEventListener("mousewheel", onWheel); 
-} else { 
-// Firefox < 17 
-window.addEventListener("MozMousePixelScroll", onWheel); 
-} 
-} else { // IE8- 
-window.attachEvent("onmousewheel", onWheel); 
-} 
+wheel = 0, 
+rotate = 0,
+stop = true;
+Go();
+$('#display').css('opacity', '1');
+$('#display').fadeOut(0);
+
+
+function Go() {
+    $('.wrapper').one('wheel', onWheel)
+}
+
+
 
 function onWheel(event){ 
+
+
 var e = event;
-if(e.deltaY < 0 && i === 0){
-    wheel = 0;
-} else if(e.deltaY > 0 && i === 8){
-    wheel = 0;
-}else {
-    wheel += e.deltaY;
+
+console.log(e);
+
+
+if(e){
+   if(e.originalEvent.wheelDelta > 0 && i === 0){
+        wheel = 0;
+        Go();
+    } else if(e.originalEvent.wheelDelta < 0 && i === 8){
+        wheel = 0;
+        Go();
+    }else {
+        wheel += e.originalEvent.wheelDelta;
+    }
+        console.log(wheel);
+    if(wheel <= -120 && i != 8){ 
+    i++; 
+    
+    Ajax(i); 
+    }else if(i != 0 && wheel >= 120){ 
+    i--; 
+    Ajax(i); 
+    }  
+    
+
 }
-    console.log(wheel);
-if(wheel >= 600 && i != 8){ 
-i++; 
-console.log(wheel); 
-Ajax(i); 
-}else if(i != 0 && wheel <= -600){ 
-i--; 
-Ajax(i); 
-} 
+
+    
 } 
 var rotate = parseInt(-45);
 
 function Ajax(i){ 
-var i = i; 
+
+
+i = i; 
 wheel = 0;
 console.log(i);
 if (i == 0 || i == 8) {
    $('#display').fadeOut();
 } else{
+    
     $('#display').fadeIn();
-    $('#display').css('display', 'flex')
+    $('#display').css('display', 'flex');
+    
+    
+}
+function getRandomInt(min, max) { 
+return Math.floor(Math.random() * (max - min)) + min; 
+} 
+$('#wrap').empty(); 
+$('#wrap').css('display', 'none'); // по окончанию загрузки страницы 
+// $('#example-1').click(function(){ // вешаем на клик по элементу с id = example-1 
+// $(document).ready(function(){ 
+// по окончанию загрузки страницы 
+$.ajax({ 
+url: 'page'+ i +'.html', // указываем URL и 
+cache: false, 
+beforeSend: function(){ 
+rotate += 360; 
+$('#line').css('transform', 'rotate('+ rotate +'deg)'); 
+$('#line').css('background', 'rgb('+ getRandomInt(0, 255) + ',' + getRandomInt(0, 255) + ',' + getRandomInt(0, 255) +')'); 
+setTimeout(Go, 500);
+
+}, 
+success: function(html) { 
+$('#wrap').html(html).fadeIn(500); 
+} 
+}); 
+// }); 
+// }); 
+};
+
+
+
+
+
+
+
+
+function AjaxE(i){ 
+
+
+this.i = i; 
+wheel = 0;
+console.log(i);
+if (i == 0 || i == 8) {
+   $('#display').fadeOut();
+} else{
+    
+    $('#display').fadeIn();
+    $('#display').css('display', 'flex');
+    
+    
 }
 function getRandomInt(min, max) { 
 return Math.floor(Math.random() * (max - min)) + min; 
@@ -143,3 +207,55 @@ $('#wrap').html(html).fadeIn(500);
 // }); 
 // }); 
 };
+
+
+
+
+
+
+$('#drop').hover(function(){
+    $('#down').fadeIn();
+    $('#down').css('display', 'flex');
+    $('#list').hover(function(){
+        if(!$("#list").is(":hover")){
+            $('#down').fadeOut(); 
+        } 
+    });
+});
+
+
+
+function More(show, hide, line) {
+    this.show = show;
+    this.hide = hide;
+    this.line = line;
+    this.helper();
+}
+
+More.prototype.helper = function () {
+    this.show.on('click', this.showProj.bind(this));
+}
+
+More.prototype.showProj = function () {
+    this.show.css('display', 'none'); 
+    this.hide.css('display', 'block');
+    $('.text__project').text('К основным');
+    this.line.css('display', 'none');
+    for(var i = 6; i < $('.content__img').length; i++) {
+        $('.content__img')[i].style.display = 'block';
+    }
+    this.hide.on('click', this.hideProj.bind(this));
+}
+
+More.prototype.hideProj = function () {
+    this.show.css('display', 'block'); 
+    this.hide.css('display', 'none');
+    $('.text__project').text('Больше проектов');
+    this.line.css('display', 'block');
+    for(var i = 6; i < $('.content__img').length; i++) {
+        $('.content__img')[i].style.display = 'none';
+    }
+    this.show.on('click', this.showProj.bind(this));
+}
+
+new More($('#showP'), $('#hideP'), $('.block__plus_second'));
