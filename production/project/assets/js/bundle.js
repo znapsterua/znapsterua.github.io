722125995a29be6b7e17085cad51430c52975f06
znapsterua <zartem_ua@mail.ru>
2016-12-27 10:12:03 +0200

    test
    test

--- production/project/assets/js/bundle.js
+++ production/project/assets/js/bundle.js
@@ -1,215 +1,78 @@
 function Modal(show, hide, modal) {
-            this.show = show,
-            this.hide = hide;
-            this.modal = modal;
-            this.helper();
-        }
+    this.show = show,
+    this.hide = hide;
+    this.modal = modal;
+    this.helper();
+}
 
-        Modal.prototype.helper = function () {
-            this.show.on('click', this.showModal.bind(this));
-        }
+Modal.prototype.helper = function () {
+    this.show.on('click', this.showModal.bind(this));
+}
 
-        Modal.prototype.showModal = function () {
-            this.show.css("display", "none");
-            this.hide.css("display", "block");
-            this.modal.fadeIn();
-            $('.project__box').css('transform', 'scale(1.2, 1.2)');
-            $('.show_hide').css('display', 'none');
-            
-            $('.hide_show').css('display', 'flex');
-            setTimeout(function() {
-                $('.hide_show').css('opacity', '1');
-                $('.project__cross').css('transform', 'rotate(360deg)')
-            }, 300);
-            this.animate(0);
-            this.hide.on('click', this.hideModal.bind(this));
-        }
+Modal.prototype.showModal = function () {
+    this.show.css("display", "none");
+    this.hide.css("display", "block");
+    this.modal.fadeIn();
+    $('.project__box').css('transform', 'scale(1.2, 1.2)');
+    $('.show_hide').css('display', 'none');
+    
+    $('.hide_show').css('display', 'flex');
+    setTimeout(function() {
+        $('.hide_show').css('opacity', '1');
+        $('.project__cross').css('transform', 'rotate(360deg)')
+    }, 300);
+    this.animate(0);
+    this.hide.on('click', this.hideModal.bind(this));
+}
 
-        Modal.prototype.animate = function (count) {
-            var i = count,
-                that = this;
+Modal.prototype.animate = function (count) {
+    var i = count,
+        that = this;
+    setTimeout(function() { 
+        $('.content__img')[i].style.opacity = 1; 
+        $('.content__img')[i].style.transform = 'scale(1, 1)'; 
+        if (i != 5) { 
+            i++; 
+            that.animate(i); 
+        } else { 
+            $('.modal__more').css('width', '60%'); 
+            $('.modal__more').css('opacity', '1');
             setTimeout(function() { 
-                console.log(i); 
-                $('.content__img')[i].style.opacity = 1; 
-                $('.content__img')[i].style.transform = 'scale(1, 1)'; 
-                if (i != 5) { 
-                    i++; 
-                    that.animate(i); 
-                } else { 
-                    $('.modal__more').css('width', '60%'); 
-                    $('.modal__more').css('opacity', '1');
-                    setTimeout(function() { 
-                        console.log('hi'); 
-                        $('.more__text').css('opacity', '1');
-                    }, 500) 
-                } 
-            }, 200) 
-        }
+                $('.more__text').css('opacity', '1');
+            }, 500) 
+        } 
+    }, 200) 
+}
 
-        Modal.prototype.hideModal = function () {
-            $('.project__cross').css('transform', 'rotate(0deg)')
-            $('.hide_show').css('opacity', '0');
-            setTimeout(function() {
-                $('.hide_show').css('display', 'none');
-                $('.show_hide').fadeIn();
-                $('.project__box').css('transform', 'scale(1, 1)');
-            },500);
-            for (var i = 0; i < 6; i++) {
-                $('.content__img')[i].style.opacity = 0; 
-                $('.content__img')[i].style.transform = 'scale(.7, .7)'; 
-            }
-            $('.modal__more').css('width', '0%'); 
-            $('.modal__more').css('opacity', '0');
-            $('.more__text').css('opacity', '0');
-            this.show.css("display", "block");
-            this.hide.css("display", "none");
-            this.modal.fadeOut();
-        }
+Modal.prototype.hideModal = function () {
+    $('.project__cross').css('transform', 'rotate(0deg)')
+    $('.hide_show').css('opacity', '0');
+    setTimeout(function() {
+        $('.hide_show').css('display', 'none');
+        $('.show_hide').fadeIn();
+        $('.project__box').css('transform', 'scale(1, 1)');
+    },500);
+    for (var i = 0; i < 6; i++) {
+        $('.content__img')[i].style.opacity = 0; 
+        $('.content__img')[i].style.transform = 'scale(.7, .7)'; 
+    }
+    $('.modal__more').css('width', '0%'); 
+    $('.modal__more').css('opacity', '0');
+    $('.more__text').css('opacity', '0');
+    this.show.css("display", "block");
+    this.hide.css("display", "none");
+    this.modal.fadeOut();
+}
 
-        new Modal($('#show'), $('#hide'), $('.modal'));
+new Modal($('#show'), $('#hide'), $('.modal'));
 
+function ShowModal(){
+    $('.modal').fadeIn()
+}
 
-
-// var i = 0,
-// wheel = 0, 
-// rotate = 0,
-// stop = true;
-// Go();
-// $('#display').css('opacity', '1');
-// $('#display').fadeOut(0);
-
-
-// function Go() {
-//     $('.wrapper').one('wheel', onWheel)
-// }
-
-
-
-// function onWheel(event){ 
-
-
-// var e = event;
-
-// console.log(e);
-
-
-// if(e){
-//    if(e.originalEvent.wheelDelta > 0 && i === 0){
-//         wheel = 0;
-//         Go();
-//     } else if(e.originalEvent.wheelDelta < 0 && i === 8){
-//         wheel = 0;
-//         Go();
-//     }else {
-//         wheel += e.originalEvent.wheelDelta;
-//     }
-//         console.log(wheel);
-//     if(wheel <= -1 && i != 8){ 
-//     i++; 
-    
-//     Ajax(i); 
-//     }else if(i != 0 && wheel >= 1){ 
-//     i--; 
-//     Ajax(i); 
-//     }  
-    
-
-// }
-
-    
-// } 
-// var rotate = parseInt(-45);
-
-// function Ajax(i){ 
-
-
-// i = i; 
-// wheel = 0;
-// console.log(i);
-// if (i == 0 || i == 8) {
-//    $('#display').fadeOut();
-// } else{
-    
-//     $('#display').fadeIn();
-//     $('#display').css('display', 'flex');
-    
-    
-// }
-// function getRandomInt(min, max) { 
-// return Math.floor(Math.random() * (max - min)) + min; 
-// } 
-// $('#wrap').empty(); 
-// $('#wrap').css('display', 'none'); // по окончанию загрузки страницы 
-// // $('#example-1').click(function(){ // вешаем на клик по элементу с id = example-1 
-// // $(document).ready(function(){ 
-// // по окончанию загрузки страницы 
-// $.ajax({ 
-// url: 'page'+ i +'.html', // указываем URL и 
-// cache: false, 
-// beforeSend: function(){ 
-// rotate += 360; 
-// $('#line').css('transform', 'rotate('+ rotate +'deg)'); 
-// $('#line').css('background', 'rgb('+ getRandomInt(0, 255) + ',' + getRandomInt(0, 255) + ',' + getRandomInt(0, 255) +')'); 
-// setTimeout(Go, 1000);
-
-// }, 
-// success: function(html) { 
-// $('#wrap').html(html).fadeIn(500); 
-// } 
-// }); 
-// // }); 
-// // }); 
-// };
-
-
-
-
-
-
-
-
-// function AjaxE(i){ 
-
-
-// this.i = i; 
-// wheel = 0;
-// console.log(i);
-// if (i == 0 || i == 8) {
-//    $('#display').fadeOut();
-// } else{
-    
-//     $('#display').fadeIn();
-//     $('#display').css('display', 'flex');
-    
-    
-// }
-// function getRandomInt(min, max) { 
-// return Math.floor(Math.random() * (max - min)) + min; 
-// } 
-// $('#wrap').empty(); 
-// $('#wrap').css('display', 'none'); // по окончанию загрузки страницы 
-// // $('#example-1').click(function(){ // вешаем на клик по элементу с id = example-1 
-// // $(document).ready(function(){ 
-// // по окончанию загрузки страницы 
-// $.ajax({ 
-// url: 'page'+ i +'.html', // указываем URL и 
-// cache: false, 
-// beforeSend: function(){ 
-// rotate += 360; 
-// $('#line').css('transform', 'rotate('+ rotate +'deg)'); 
-// $('#line').css('background', 'rgb('+ getRandomInt(0, 255) + ',' + getRandomInt(0, 255) + ',' + getRandomInt(0, 255) +')'); 
-
-// }, 
-// success: function(html) { 
-// $('#wrap').html(html).fadeIn(500); 
-// } 
-// }); 
-// // }); 
-// // }); 
-// };
-
-
-
+function HideModal(){
+    $('.modal').fadeOut()
+}
 
 
 
@@ -319,8 +182,11 @@ function HideSuccess() {
     $('.success-modal').fadeOut();
 }
 
-$(document).ready(function(){
+
+
+function Slick(){
   $('.slider__slick').slick({
+    autoplay: true,
     arrows: false,
     dots: true,
     infinite: true,
@@ -328,5 +194,9 @@ $(document).ready(function(){
     variableWidth: true,
     slidesToScroll: 1
   });
-});
+};
+
+if(document.querySelector('.slider__slick')){
+    Slick();
+}
 
--- production/project/assets/js/main.js
+++ production/project/assets/js/main.js
@@ -1,16 +1,17 @@
-
-
 var i = 0,
-wheel = 0, 
-rotate = 0,
-stop = true;
+    wheel = 0, 
+    rotate = 0,
+    stop = true;
 
 if (document.documentElement.clientWidth >= 1100) {
     Go();
 }
 
-$('#display').css('opacity', '1');
-$('#display').fadeOut(0);
+if(document.querySelector('#display')){
+    $('#display').css('opacity', '1');
+    $('#display').fadeOut(0);
+}
+
 
 
 function Go() {
@@ -24,8 +25,6 @@ function onWheel(event){
 
 var e = event;
 
-console.log(e);
-
 
 if(e){
    if(e.originalEvent.wheelDelta > 0 && i === 0){
@@ -37,62 +36,51 @@ if(e){
     }else {
         wheel += e.originalEvent.wheelDelta;
     }
-        console.log(wheel);
     if(wheel <= -1 && i != 8){ 
-    i++; 
-    
-    Ajax(i); 
+        i++; 
+        Ajax(i); 
     }else if(i != 0 && wheel >= 1){ 
-    i--; 
-    Ajax(i); 
+        i--; 
+        Ajax(i); 
     }  
-    
-
-}
-
-    
+}  
 } 
 var rotate = parseInt(-45);
 
-function Ajax(i){ 
+function Ajax(i, type = true){ 
+    this.type = type;
+    this.i = i; 
+    wheel = 0;
+    if (i == 0 || i == 8) {
+       $('#display').fadeOut();
+    } else{
+        $('#display').fadeIn();
+        $('#display').css('display', 'flex');
+         
+    }
+    function getRandomInt(min, max) { 
+        return Math.floor(Math.random() * (max - min)) + min; 
+    } 
 
+    $('#wrap').empty(); 
+    $('#wrap').css('display', 'none'); 
 
-i = i; 
-wheel = 0;
-console.log(i);
-if (i == 0 || i == 8) {
-   $('#display').fadeOut();
-} else{
-    
-    $('#display').fadeIn();
-    $('#display').css('display', 'flex');
-    
-    
-}
-function getRandomInt(min, max) { 
-return Math.floor(Math.random() * (max - min)) + min; 
-} 
-$('#wrap').empty(); 
-$('#wrap').css('display', 'none'); // по окончанию загрузки страницы 
-// $('#example-1').click(function(){ // вешаем на клик по элементу с id = example-1 
-// $(document).ready(function(){ 
-// по окончанию загрузки страницы 
-$.ajax({ 
-url: 'page'+ i +'.html', // указываем URL и 
-cache: false, 
-beforeSend: function(){ 
-rotate += 360; 
-$('#line').css('transform', 'rotate('+ rotate +'deg)'); 
-$('#line').css('background', 'rgb('+ getRandomInt(0, 255) + ',' + getRandomInt(0, 255) + ',' + getRandomInt(0, 255) +')'); 
-setTimeout(Go, 1000);
+    $.ajax({ 
+        url: 'ajax/page'+ i +'.html', 
+        cache: false, 
+        beforeSend: function(){ 
+            rotate += 360; 
+            $('#line').css('transform', 'rotate('+ rotate +'deg)'); 
+            $('#line').css('background', 'rgb('+ getRandomInt(0, 255) + ',' + getRandomInt(0, 255) + ',' + getRandomInt(0, 255) +')'); 
+            if(type === true){
+                setTimeout(Go, 1000);
+            }
+        }, 
 
-}, 
-success: function(html) { 
-$('#wrap').html(html).fadeIn(500); 
-} 
-}); 
-// }); 
-// }); 
+        success: function(html) { 
+            $('#wrap').html(html).fadeIn(500); 
+        } 
+    }); 
 };
 
 
@@ -102,43 +90,4 @@ $('#wrap').html(html).fadeIn(500);
 
 
 
-function AjaxE(i){ 
-
-
-this.i = i; 
-wheel = 0;
-console.log(i);
-if (i == 0 || i == 8) {
-   $('#display').fadeOut();
-} else{
-    
-    $('#display').fadeIn();
-    $('#display').css('display', 'flex');
-    
-    
-}
-function getRandomInt(min, max) { 
-return Math.floor(Math.random() * (max - min)) + min; 
-} 
-$('#wrap').empty(); 
-$('#wrap').css('display', 'none'); // по окончанию загрузки страницы 
-// $('#example-1').click(function(){ // вешаем на клик по элементу с id = example-1 
-// $(document).ready(function(){ 
-// по окончанию загрузки страницы 
-$.ajax({ 
-url: 'page'+ i +'.html', // указываем URL и 
-cache: false, 
-beforeSend: function(){ 
-rotate += 360; 
-$('#line').css('transform', 'rotate('+ rotate +'deg)'); 
-$('#line').css('background', 'rgb('+ getRandomInt(0, 255) + ',' + getRandomInt(0, 255) + ',' + getRandomInt(0, 255) +')'); 
-
-}, 
-success: function(html) { 
-$('#wrap').html(html).fadeIn(1000); 
-} 
-}); 
-// }); 
-// }); 
-};
 
